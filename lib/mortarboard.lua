3ed2d92172a26dab90a4c5ec0af4d159cea73721
Pablo Lins <pablo.lins@ideais.com.br>
2015-02-27 11:47:57 -0300

    added detailed comments

--- lib/mortarboard.lua
+++ lib/mortarboard.lua
@@ -3,9 +3,18 @@ require('./util')
 local http   = require('http')
 local router = require('./router')
 
+-- return the handler for all http requests.
+-- @param app_middlewares: 
+--     [type]: list of functions
+--     [desc]: holds registered middlewares with app.use()
+-- @param routes
+--     [type]: list of functions
+--     [desc]: holds registered middlewares with
+--             app.get(), app.post(), app.put() and app.delete()
+-- @returns function
 local requestHandler = function (app_middlewares, routes)
   return function(req, res)
-    -- list of all middlewares that will
+    -- list of all middlewares that together will
     -- sequentially compute the request
     local request_chain = {}
     request_chain = table.concatenate(request_chain, app_middlewares)
@@ -29,17 +38,29 @@ local requestHandler = function (app_middlewares, routes)
   end
 end
 
+-- creates an application instance.
+-- @returns table, containing all the app methods
 local createApp = function()
   local app       = {} -- hash
   app.middlewares = {} -- list
   app.routes      = {} -- list
 
-  -- register a application middleware, ORDER MATTERS!
+  -- register a application middleware, ORDER MATTERS.
+  -- @param middleware:
+  --     [type]: function
+  --     [desc]: middleware to be registered within the app
   app.use = function (middleware)
     table.insert(app.middlewares, middleware)
   end
 
- -- auxiliary methods for registering new routes
+  -- auxiliary methods for registering new routes.
+  -- @param path:
+  --     [type]: string
+  --     [desc]: path to resource, ex: '/api/customer'
+  -- @param route_middlewares:
+  --     [type]: list of functions
+  --     [desc]: middlewares to be executed after
+  --             the app middlewares for the given path
   app.get = function (path, route_middlewares)
     router.create(routes, 'GET', path, route_middlewares)
   end
@@ -56,7 +77,9 @@ local createApp = function()
     router.create(routes, 'DELETE', path, route_middlewares)
   end 
 
-  -- creates and binds a server to a port
+  -- creates and binds a server to a port.
+  -- @param port:
+  --     [type]: number
   app.listen = function (port)
     local server = http.createServer(
       requestHandler(app.middlewares, app.routes)
--- lib/router.lua
+++ lib/router.lua
@@ -1,8 +1,21 @@
 local router = {}
 
-router.create = function(routes, method, path, ...)
-  if type(method) ~= 'string' then
-    return error('1st argument "method" should be a string, got ' .. type(method) .. 'instead')
+-- creates a route for a given a HTTP method and path
+-- @param routes: 
+--     [type]: list of functions
+--     [desc]: holds registered middlewares with app.VERB()
+-- @param request_method:
+--     [type]: string
+--     [desc]: HTTP method associated with the route
+-- @param path:
+--     [type]: string
+--     [desc]: path to resource, ex: '/api/customer'
+-- @param ...:
+--     [type]: list of functions
+--     [desc]: list holding the middlewares for the given path
+router.create = function(routes, request_method, path, ...)
+  if type(request_method) ~= 'string' then
+    return error('1st argument "method" should be a string, got ' .. type(request_method) .. 'instead')
   end
 
   if type(path) ~= 'string' then
@@ -10,7 +23,7 @@ router.create = function(routes, method, path, ...)
   end
 
   -- TODO: inspect arguments to see if theres 
-  -- anything theres not a function
+  -- anything that is not a function
   local middlewares = table.flatten({...})
 
   table.insert(routes, {
@@ -18,11 +31,21 @@ router.create = function(routes, method, path, ...)
     middlewares = middlewares, -- list of functions with (req, res, next) as signature
     method      = method       -- string with http method
   })
-
 end
 
+-- finds the matched route for a given method and path.
+-- @param routes: 
+--     [type]: list of functions
+--     [desc]: holds registered middlewares with app.VERB()
+-- @param request_method:
+--     [type]: string
+--     [desc]: HTTP method to be matched
+-- @param path:
+--     [type]: string
+--     [desc]: path to be matched, ex: '/api/customer'
+-- @returns table holding the matched route, created with route.create
 router.match = function(routes, request_method, request_path)
-  return table.first(routes, function(route)
+  return table.find(routes, function(route)
     return route.path == request_path and route.method == request_method
   end)
 end
