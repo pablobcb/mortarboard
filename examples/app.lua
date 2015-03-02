82f3d22e64f2a0e63e6888e6aa8e3e1255f87102
Pablo Lins <pablo.lins@ideais.com.br>
2015-03-02 20:23:49 -0300

    examples now showing all parsers

--- examples/app.lua
+++ examples/app.lua
@@ -4,15 +4,24 @@ local mb         = require('../lib/mortarboard')
 local app = mb()
 
 
+-- to parse jason and etc
 app.use(bodyparser())
 
-app.post('/foo/:p1/bar/:jeff/:koilos', function(req, res)
+app.post('/foo/:a/bar', function(req, res)
+  p("path parameteres: " .. req.params.a)
+
+  p('querystring parameters:')
+  p(req.query)
+
+  p('raw body:')
+  p(req.raw_body)
+
+  p('json body:')
+  p(req.body)
+
   res.json(req.body)
 end)
 
-app.get('/jeff',function(req, res)
-  res.send('UHUL')
-end)
 
 local port = 8000
 app.listen(port)
